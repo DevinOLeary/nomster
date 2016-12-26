a5b33f9e5bb582440b7e73f484e0c1470c3f32bb
Devin O'Leary <djoleary07@gmail.com>
2016-12-26 16:37:26 +0000

    removed items from db

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @places = Place.all
     @places = Place.order("name").page(params[:page]).per(3)
@@ -9,7 +10,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new(place_params)
+    @place = current_user.place.create(place_params)
     if @place.save
       redirect_to root_path
     else
