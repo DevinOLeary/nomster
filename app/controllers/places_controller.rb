4f1ceb03509d1cab6981333fab75513499f3adbd
Devin O'Leary <djoleary07@gmail.com>
2016-12-28 20:15:44 +0000

    styling error

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,28 +1,31 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  # Requires a user to be logged in in order to carry out listed
+  # processes.
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  # View for list of places
   def index
     @places = Place.all
     @places = Place.order("name").page(params[:page]).per(3)
   end
-
+# view for new place form
   def new
     @place = Place.new
   end
-
+# creates new place
   def create
     @place = current_user.place.create(place_params)
-    if @place.save
+    if @place.valid?
       redirect_to root_path
     else
       flash.now[:error] = 'We need more information.'
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
-
+# show place when you click on title
   def show
     @place = Place.find(params[:id])
   end
-
+# brings up a filled out edit form
   def edit
     @place = Place.find(params[:id])
 
@@ -30,7 +33,7 @@ class PlacesController < ApplicationController
       return render text: "You don't have permission to do this... Who do you think you are?", status: :forbidden
     end
   end
-
+# Updates edited form
   def update
     @place = Place.find(params[:id])
     if @place.user != current_user
@@ -38,12 +41,18 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
-    
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
-
+# Deletes place 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: "You are not allowed to do this", status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
