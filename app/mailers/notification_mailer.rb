bdfaf963de59c5f6a0e38385befe6622e6637745
Devin O'Leary <djoleary07@gmail.com>
2016-12-31 17:22:37 +0000

    production mailer setup

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,11 +2,10 @@ class NotificationMailer < ActionMailer::Base
   default from: "<no-reply@nomsterapp.com>"
 
   def comment_added(comment)
-    # @place = comment.place
-    # @place_owner = @place.user
-    # mail(to: @place_owner.email,
-    #       subject: "Someone just commented on #{@place.name}!")
-    mail(to: "djodev.tester@gmail.com",
-          subject: "You got mail!")
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+          subject: "Someone just commented on #{@place.name}!")
+
   end
 end
