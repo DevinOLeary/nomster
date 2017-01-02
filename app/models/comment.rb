56d8ba4d4a593f90cfce93263c4ceca3ea65886f
Devin O'Leary <djoleary07@gmail.com>
2017-01-02 03:24:41 +0000

    user stats

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
-  after_create :comment_send_email 
+  after_create :comment_send_email
 
   RATINGS = {
-    'one star' => '1_star',
-    'two stars' => '2_stars',
-    'three stars' => '3_stars',
-    'four stars' => '4_stars',
-    'five stars' => '5_stars'
+    'One Star' => '1_star',
+    'Two Stars' => '2_stars',
+    'Three Stars' => '3_stars',
+    'Four Stars' => '4_stars',
+    'Five Stars' => '5_stars'
   }
 
   def humanized_rating
