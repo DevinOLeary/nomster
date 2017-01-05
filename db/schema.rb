3f2e5e339f86ac1f9d9a87e3fc5ecbdbeaaecba3
Devin O'Leary <djoleary07@gmail.com>
2017-01-05 16:54:02 +0000

    pic db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230205932) do
+ActiveRecord::Schema.define(version: 20170105164929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,28 @@ ActiveRecord::Schema.define(version: 20161230205932) do
     t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "phone"
+    t.text     "hours"
+    t.string   "website"
   end
 
   add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
+
+  create_table "profiles", force: true do |t|
+    t.string   "pic"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_pictures", force: true do |t|
+    t.string   "user_pic"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "user_pictures", ["user_id"], name: "index_user_pictures_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -62,9 +81,11 @@ ActiveRecord::Schema.define(version: 20161230205932) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "user_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["user_id"], name: "index_users_on_user_id", using: :btree
 
 end
